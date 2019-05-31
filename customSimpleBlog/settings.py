e32405b04a92727821da42d5f326218580c493bc
Nikita <nikita.chefonov@mail.ru>
2019-06-01 02:05:38 +0700

    Server commit

--- customSimpleBlog/settings.py
+++ customSimpleBlog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ayvyx4hrjbcd@%c3%kv%=n(uzob9e=9*mgjbn-+^#ug)efa4(f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,4 +119,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') # пустая папка, сюда будет собирать статику collectstatic
 STATIC_URL = '/static/'
